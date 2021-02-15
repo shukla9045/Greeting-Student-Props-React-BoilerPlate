aee71282f436e8fcc594087d0eb008129e190cf9
Saurabh Shukla <73607833+shukla9045@users.noreply.github.com>
2021-02-15 17:23:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import '../styles/App.css';
-import Welcome from './Welcome';
+import "../styles/App.css";
+import Welcome from "./Welcome";
 
 const App = () => {
   return (
-    <Welcome name="" />
-  )
-}
-
+    <>
+      <Welcome name={"Saurabh"} />
+    </>
+  );
+};
 
 export default App;
